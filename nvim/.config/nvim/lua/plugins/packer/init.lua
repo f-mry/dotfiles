5c454b1231aeeefb4d44f99d4deeaa9c9149a728
Farhan <farhanmry@yahoo.com>
2022-05-10 23:44:20 +0900

    nvim: add rose pine 👎

--- nvim/.config/nvim/lua/plugins/packer/init.lua
+++ nvim/.config/nvim/lua/plugins/packer/init.lua
@@ -198,6 +198,51 @@ packer.startup(function(use)
     use {'folke/tokyonight.nvim'}
     use {'christianchiarulli/nvcode-color-schemes.vim'}
     use {'Shatur/neovim-ayu'}
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        tag = 'v1.*',
+        config = function ()
+            require('rose-pine').setup({
+                ---@usage 'main'|'moon'
+                dark_variant = 'main',
+                bold_vert_split = true,
+                dim_nc_background = false,
+                disable_background = false,
+                disable_float_background = false,
+                disable_italics = true,
+                ---@usage string hex value or named color from rosepinetheme.com/palette
+                groups = {
+                    background = 'base',
+                    panel = 'surface',
+                    border = 'highlight_med',
+                    comment = 'muted',
+                    link = 'iris',
+                    punctuation = 'subtle',
+
+                    error = 'love',
+                    hint = 'iris',
+                    info = 'foam',
+                    warn = 'gold',
+
+                    headings = {
+                        h1 = 'iris',
+                        h2 = 'foam',
+                        h3 = 'rose',
+                        h4 = 'gold',
+                        h5 = 'pine',
+                        h6 = 'foam',
+                    }
+                    -- or set all headings at once
+                    -- headings = 'subtle'
+                },
+                -- Change specific vim highlight groups
+                highlight_groups = {
+                    ColorColumn = { bg = 'rose' }
+                }
+            })
+        end
+    })
 
 end)
 
